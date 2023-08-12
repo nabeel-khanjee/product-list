b7c92dff6db414622cf8ea79b4dec33773c773fb
Nabeel <nabeel@merisehat.pk>
2023-08-12 15:47:02 +0500

    flutter flavor

---
+++ lib/flavors.dart
@@ -0,0 +1,22 @@
+enum Flavor {
+  prod,
+  staging,
+}
+
+class F {
+  static Flavor? appFlavor;
+
+  static String get name => appFlavor?.name ?? '';
+
+  static String get title {
+    switch (appFlavor) {
+      case Flavor.prod:
+        return 'AT Care';
+      case Flavor.staging:
+        return 'AT Care Staging';
+      default:
+        return 'title';
+    }
+  }
+
+}
