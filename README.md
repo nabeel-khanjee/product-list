e9d289235fff9d0339f0afac48d4ed8e07dd179c
Nabeel <nabeel@merisehat.pk>
2023-10-17 20:38:35 +0500

    pkg name chnage

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# provider_app
+# softtecg_test
 
 A new Flutter project.
 
--- android/app/src/main/kotlin/com/example/patient_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/patient_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.provider_app
+package com.example.softtecg_test
 
 import io.flutter.embedding.android.FlutterActivity
 
